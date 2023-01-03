dd82c18596315df7cbcc57c49c2cb7a38c3fcf5c
FacuBc <facundobesse@gmail.com>
2023-01-03 11:08:23 -0300

    ejercicio add

---
+++ ejercicio-02/ejercicio_dos.js
@@ -0,0 +1,15 @@
+
+const libro = {
+    titulo:"milanium",
+    autor:"alguien",
+    fecha: new Date().getDate,
+    URL:"WWWW.URL.COM"
+}
+
+let lista = [
+    "Facu",
+    33,
+    true,
+    new Date().getDate,
+    libro
+]
