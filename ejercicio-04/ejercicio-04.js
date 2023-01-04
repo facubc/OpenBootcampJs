44daa9b1a562cb27bd957feb8d2729435653b5ce
FacuBc <facundobesse@gmail.com>
2023-01-04 00:34:21 -0300

    ejercicio cuatro

---
+++ ejercicio-04/ejercicio-04.js
@@ -0,0 +1,19 @@
+let nombre = "Facu";
+
+let lastName ="Bessé";
+
+let estudiante = nombre + " " + lastName;
+
+let estudianteMayus = estudiante.toLocaleUpperCase;
+
+let estudianteMunis = estudiante.toLocaleLowerCase;
+
+let largo = estudiante.length;
+
+let firstChar = nombre.charAt(0);
+
+let firstCharLasName = lastName.charAt(lastName.length - 1);
+
+let sinEspacios = estudiante.trim();
+
+let contiene = estudiante.includes("Facu");
