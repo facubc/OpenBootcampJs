28928b3bb17c43c997f0353c0ec778822da97c05
FacuBc <facundobesse@gmail.com>
2023-01-17 11:37:50 -0300

    ejercicio 07.01

---
+++ ejercicio-07/ejercicio-07-01.js
@@ -0,0 +1,10 @@
+
+const familArray = ["Emi","Fer","Cary","Facu"];
+const familySet = new Set(familArray);
+console.log(familySet);
+
+familySet.add("Facu");
+console.log(familySet);
+
+familySet.add("JavaScript");
+console.log(familySet)
